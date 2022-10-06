635963a9a0e41e666874c53ed1567c0d0b900bf5
vo0922 <vo0922@naver.com>
2022-10-06 22:33:55 +0900

    tagList Delete

--- public/javascripts/schedule/tagModal.js
+++ public/javascripts/schedule/tagModal.js
@@ -1,11 +1,24 @@
 let modal = document.getElementById('scheduleModal');
 
 function tagSelected(e) {
+    let tagHash = e.value.split('#');
+
     if (window.event.keyCode == 13) {
-        let tagList = document.getElementById('tagList');
-        addTagList(tagList.value);
-        tagList.value='';
+        let tagList = document.getElementById('tagList').value;
+        if (!tagList) {
+            return
+        }
+        if (!tagHash[0]) {
+            return addTagList(tagHash[1])
+        }
+        addTagList(tagList);
     } else {
+        if (!e.value) {
+            return modal.style.display = "none";
+        }
+        if (!tagHash[0]) {
+            return select(tagHash[1])
+        }
         select(e.value)
     }
 }
@@ -23,7 +36,9 @@ function select(text) {
             let tagDiv = '';
             if (!res.data)
                 return modal.style.display = "none";
-            modal.style.display = "block";
+            else {
+                modal.style.display = "block";
+            }
 
             res.data.map((data) => {
                 tagDiv += `<p onclick="addTagList(this.innerText)">${data.name}</p>`
@@ -37,10 +52,26 @@ function select(text) {
 }
 
 function addTagList(text) {
-    let newTag = document.createElement('div')
-    newTag.innerText = text;
-    newTag.className = 'tagList';
+    let newTag = createElements(text);
+    let leftTag = document.getElementsByClassName(text)[0];
+    if (leftTag) {
+        return alert("이미 태그가 존재합니다.");
+    }
     let tagDiv = document.getElementById('tagList');
     tagDiv.before(newTag);
+    tagDiv.value = '';
     modal.style.display = "none";
+}
+
+function createElements(text) {
+    let newTag = document.createElement('div')
+    newTag.innerText = text;
+    newTag.className = `tagList ${text}`;
+    let newDelete = document.createElement('i');
+    newDelete.className = 'fa-regular fa-circle-xmark';
+    newDelete.onclick = function () {
+        newTag.remove();
+    }
+    newTag.appendChild(newDelete);
+    return newTag;
 }
