980361dd8ac773e4fbea2cafa384216225604908
ssoong12 <ssoong12@naver.com>
2022-10-06 11:29:31 +0900

    header 팔레트 기능

---
+++ public/javascripts/layout/header.js
@@ -0,0 +1,57 @@
+let clickUser = document.getElementById("drop-user-content")
+let clickColor = document.getElementById("drop-color-content")
+
+/*=============== User Profile 클릭 시 상세 정보 표시 ===============*/
+function dp_menu() {
+    if (clickUser.style.display === "none") {
+        clickUser.style.display = "block"
+        clickColor.style.display = "none"
+
+    } else {
+        clickUser.style.display = "none"
+    }
+}
+/*=============== 팔레트 클릭 시 header 색상 바꾸기 상세 정보 표시 ===============*/
+function dp_headerColor() {
+    if (clickColor.style.display === "none") {
+        clickColor.style.display = "block"
+        clickUser.style.display = "none"
+    } else {
+        clickColor.style.display = "none"
+    }
+}
+
+var selectedColor; //선택한 컬러값 저장할 변수
+
+window.onload = function(){
+    init();
+}
+
+function init(){
+    var colorChip = ["#f89b00", "#f7e600", "#7dbad5", 
+                    "#e091a9", "#86e6c2", "#81c147", 
+                    "#9941ec", "#ff93f1", "#fd3b85",
+                    "#ffd54a", "#6eff4a", "#5ab9c5"]; //색상코드
+    var tag = "";
+    for(i=0; i<colorChip.length; i++){  //colorBox 의 id명을 색상명으로 지정해주기.
+        tag += "<div id="+colorChip[i]+" class='colorBox' onclick='colorSet(this)'></div>";
+    }
+    document.getElementById("colorList").innerHTML = tag;
+
+    var colorBoxList = document.getElementsByClassName("colorBox");
+    for(i=0; i<colorBoxList.length; i++){
+        colorBoxList[i].style.background = colorBoxList[i].id;  //id인 색상명을 colorBox의 배경색으로 지정해주기
+    }
+}
+
+// onclick event
+function colorSet(colorPick){
+    document.querySelector("#header").style.background = colorPick.id;  //배경색을 선택한 색상박스의 id 값으로 지정해주기
+
+    if(selectedColor != null){  
+        document.getElementById(selectedColor).className = document.getElementById(selectedColor).className.replace(" selected", "");
+    }
+    document.getElementById(colorPick.id).className += " selected";
+    selectedColor = colorPick.id;
+}
+
