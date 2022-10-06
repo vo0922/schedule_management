9acccc0cde1caafa34430b3b195cda56e9f7c4bb
vo0922 <71069665+vo0922@users.noreply.github.com>
2022-10-06 17:39:23 +0900

    tagList modify

--- public/javascripts/schedule/tagModal.js
+++ public/javascripts/schedule/tagModal.js
@@ -1,26 +1,32 @@
 let modal = document.getElementById('scheduleModal');
 
 function tagSelected(e) {
-    select(e.value)
+    if (window.event.keyCode == 13) {
+        let tagList = document.getElementById('tagList');
+        addTagList(tagList.value);
+        tagList.value='';
+    } else {
+        select(e.value)
+    }
 }
 
 function select(text) {
-    let tagList = document.getElementById('tagList');
+    let tagList = document.getElementById('tagModalList');
     const url = '/tag/change'
     $.ajax({
         type: 'post',
-        url : url,
-        data : {
-            text:text
+        url: url,
+        data: {
+            text: text
         },
-        success: function (res){
+        success: function (res) {
             let tagDiv = '';
-            if(!res.data)
+            if (!res.data)
                 return modal.style.display = "none";
             modal.style.display = "block";
 
-            res.data.map((data)=>{
-                tagDiv += `<p onclick="addTagList(this)">${data.name}</p>`
+            res.data.map((data) => {
+                tagDiv += `<p onclick="addTagList(this.innerText)">${data.name}</p>`
             })
             tagList.innerHTML = tagDiv
         },
@@ -30,12 +36,11 @@ function select(text) {
     })
 }
 
-function addTagList(e) {
-    let tagList = document.getElementsByClassName('tagList');
+function addTagList(text) {
     let newTag = document.createElement('div')
-    newTag.innerText = e.innerText;
+    newTag.innerText = text;
     newTag.className = 'tagList';
-    tagList.append(newTag);
     let tagDiv = document.getElementById('tagList');
-    tagDiv.append(tagDiv);
+    tagDiv.before(newTag);
+    modal.style.display = "none";
 }
