dd55d7ecaac9a2d4af1de68eb32f32a49df2b22e
ssoong12 <ssoong12@naver.com>
2022-10-12 13:52:23 +0900

    calender Plus Button 생성
    일정 생성, 카테고리 두 개의 항목이 뜰 수 있는 혼합 버튼입니다.

--- public/javascripts/calendar/calendar.js
+++ public/javascripts/calendar/calendar.js
@@ -23,3 +23,13 @@ window.onclick = function (event) {
     }
 }
 
+/*=============== + 버튼 클릭 시 선택 사항 표시 ===============*/
+let plus = document.getElementById("plus_menu")
+
+function plus_menu() {
+    if (plus.style.display === "none") {
+        plus.style.display = "block"
+    } else {
+        plus.style.display = "none"
+    }
+}
