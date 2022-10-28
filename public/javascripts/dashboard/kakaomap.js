372c7bbf029aec47680da712295a653cf7fe5304
vo0922 <vo0922@naver.com>
2022-10-28 20:40:27 +0900

    kakaomapCustom

--- public/javascripts/dashboard/kakaomap.js
+++ public/javascripts/dashboard/kakaomap.js
@@ -11,19 +11,22 @@ let mapContainer = document.getElementById('map'), // 지도를 표시할 div
 let map = new kakao.maps.Map(mapContainer, mapOption);
 var bounds = new kakao.maps.LatLngBounds();
 function addMarker(position, idx, title) {
-
-    var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png', // 마커 이미지 url, 스프라이트 이미지를 씁니다
-        imageSize = new kakao.maps.Size(36, 37),  // 마커 이미지의 크기
-        imgOptions =  {
-            spriteSize : new kakao.maps.Size(36, 691), // 스프라이트 이미지의 크기
-            spriteOrigin : new kakao.maps.Point(0, (idx*46)+10), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
-            offset: new kakao.maps.Point(13, 37) // 마커 좌표에 일치시킬 이미지 내에서의 좌표
-        },
-        markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imgOptions),
         marker = new kakao.maps.Marker({
             position: position, // 마커의 위치
-            image: markerImage
         });
+    var content = '<div class="customoverlay">' +
+        `  <a href="https://map.kakao.com/link/map/${title},${position.Ma},${position.La}" target="_blank">` +
+        `    <span class="title">${title}</span>` +
+        '  </a>' +
+        '</div>';
+
+    var customOverlay = new kakao.maps.CustomOverlay({
+        map: map,
+        position: position,
+        content: content,
+        yAnchor: 1
+    });
+
     bounds.extend(position);
     marker.setMap(map); // 지도 위에 마커를 표출합니다
     markers.push(marker);  // 배열에 생성된 마커를 추가합니다
