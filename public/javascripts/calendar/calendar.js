d0d89e406f35b710c3a9587d9be53cb40d43e3b3
ssoong12 <ssoong12@naver.com>
2022-11-16 17:13:10 +0900

    캘린더 카테고리 사이드바 토글
    모바일에서도 캘린더 확인이 가능하도록 카테고리 영역 사이드바 토글 효과를 추가하였습니다.

--- public/javascripts/calendar/calendar.js
+++ public/javascripts/calendar/calendar.js
@@ -11,3 +11,29 @@ function plus_menu() {
         plus.style.display = "none"
     }
 }
+
+/**
+ * 담당자 : 이승현
+ * 함수 설명 : 캘린더 페이지 사이드바를 접었다, 폈다 토글하는 함수
+ * 기능 설명 : 카테고리 영역과 카테고리 탭 영역을 동적으로 접었다, 폈다 할 수 있는 기능입니다.
+ */
+function sidebarToggle() {
+    const sidebar = document.getElementById("mySidebar")
+    const classes = sidebar.classList
+    
+    const result = classes.toggle("close")
+    
+  // [<<]일 때
+  if(result === true){
+    document.getElementById("mySidebar").style.width = "0px";
+    document.getElementById("calendar_section2").style.margin = "0px auto";
+    document.getElementById("sidebar_toggle_btn").style.left = "0px";
+    document.getElementById("openbtn_i").style.transform = "rotate(0deg)"
+    // [>>]일 때
+  }else {
+    document.getElementById("mySidebar").style.width = "27.7%";
+    document.getElementById("calendar_section2").style.margin = "0px auto";
+    document.getElementById("sidebar_toggle_btn").style.left = "23.2%";
+    document.getElementById("openbtn_i").style.transform = "rotate(180deg)"
+  }
+}
